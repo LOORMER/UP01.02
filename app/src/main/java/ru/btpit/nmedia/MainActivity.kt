d92ff1225a715fd9cd0118a842b9e40ae24be733
DaniilDruz <128057865+LOORMER@users.noreply.github.com>
2024-02-22 13:48:37 +0300

    task7

--- app/src/main/java/ru/btpit/nmedia/MainActivity.kt
+++ app/src/main/java/ru/btpit/nmedia/MainActivity.kt
@@ -3,13 +3,9 @@ package ru.btpit.nmedia
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.ImageButton
-import android.widget.TextView
 import ru.btpit.nmedia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-
-
     var present_value_int = 0
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         var count = true
-        val Post = post(
+        val Post = Post(
             0,
             "ГБПОУ ВО &quot;БТПИТ&quot;",
             "✅ Подать заявление на участие в дистанционном электронном голосовании до 11 марта 2024 года✅ Выбрать удобный избирательный участок для голосования и подать заявление до 11 марта 2024 года✅ Узнайте о возможностях для избирателей на «Госуслугах»",
@@ -25,47 +21,35 @@ class MainActivity : AppCompatActivity() {
             0,
             0,
             false
-
-
-
         )
         with(binding)
         {
             textViewHeader.text = Post.header
             textViewContent.text = Post.content
-            textViewDateTime.text = Post.datatime
-            textViewLike.text = Post.like.toString()
-            textViewRepost.text = Post.repost.toString()
+            textViewDateTime.text = Post.dataTime
+            textViewLike.text = toStringFromNumb(Post.amountlike)
+            textViewRepost.text = toStringFromNumb(Post.amountrepost)
 
 
             imagebutn.setOnClickListener {
                 if (count) {
-                    textViewLike.setText("1")
                     imagebutn.setBackgroundResource(R.drawable.redlikes)
                 } else {
-                    textViewLike.setText("0")
                     imagebutn.setBackgroundResource(R.drawable.likes)
                 }
                 count = count.not()
             }
         }
 
-
-
-        var text11 = findViewById<TextView>(R.id.textViewRepost)
-        var imagebutton2 = findViewById<ImageButton>(R.id.imagebutn2)
-        imagebutton2.setOnClickListener {
-            present_value_int +=1;
-            text11.setText(likecount(present_value_int));
         }
     }
 
-    fun likecount(count: Int): String
+    fun toStringFromNumb(count: Int): String
     {
         return when(count)
         {
             in 0 .. 999 -> count.toString()
-            in 1000 .. 1000000 -> count.toString()
+            in 1000 .. 1000000 -> ((count/100).toFloat()/10).toString() + "К"
             in 1000000 .. 1000000000 -> count.toString()
             else -> "Более млрд"
         }
---
+++ app/src/main/java/ru/btpit/nmedia/Repository.kt
@@ -0,0 +1,33 @@
+package ru.btpit.nmedia
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+interface PostRepository {
+    fun get(): LiveData<Post>
+    fun like()
+}
+
+class PostRepositoryInMemoryImpl : PostRepository {
+    public var post = Post(
+        id = 1,
+        header = "ГПБОУ ВО БТПИТ",
+        content = "15 февраля на базе 1 и 3 корпусов ГБПОУ ВО «БТПИТ» прошли торжественные митинги, посвященные 35-й годовщине со дня вывода советских войск из Республики Афганистан с поднятием государственного флага и возложением цветов к «Деревьям Памяти».\\nКаштаны были посажены на территории учебного корпуса № 1 и 3 в честь воинов-интернационалистов, которые учились в нашем техникуме.\\nСтуденты почтили память участников войн и конфликтов минутой молчания.",
+        dataTime = "21 февраля в 19:12",
+        isLike = false,
+        amountlike = 999,
+        amountrepost = 15
+    )
+    private val data = MutableLiveData(post)
+
+    override fun get(): LiveData<Post> = data
+    override fun like() {
+        post = post.copy(likedByMe = !post.isLike)
+        data.value = post
+    }
+}
+class PostViewModel : ViewModel() {
+    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    val data = repository.get()
+    fun like() = repository.like()
+}
